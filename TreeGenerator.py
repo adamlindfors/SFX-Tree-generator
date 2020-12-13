edf6e0594f479db94afbbd01a034755f52835d8e
Adam <adamlindfors@live.se>
2020-12-13 16:57:59 +0100

    Added possibility for multiple trees

--- TreeGenerator.py
+++ TreeGenerator.py
@@ -16,31 +16,163 @@ import random
 
 
 cmds.file(force=True, newFile=True)
-curves = []
-curvesStack = []
-directionStack = []
-positionStack = []
-directions = []
-points = []
 leafs = []
 flowers = []
-HEIGHT = 1
 leafCount = 0
 blossomCount = 0
 
 
-currentPos = dt.Vector(0, 0, 0)
-currentDir = dt.Vector(0, 1, 0)
+class Tree:
+    def __init__(self, startPos):
+        self.lSys = []
+        self.startPos = startPos
+        self.poly = 0
 
-points.append(currentPos)
+        self.currentPos = self.startPos
+        self.currentDir = dt.Vector(0, 1, 0)
+        self.curves = []
+        self.curves = []
+        self.curvesStack = []
+        self.directionStack = []
+        self.positionStack = []
+        self.points = []
+        self.points.append(self.startPos)
+        self.polys = []
+        self.leafs = []
+        self.flowers = []
+        self.numIters = 0
 
-# Rewrite rules
+    def createLSystem(self, numIters, axiom):
+        startString = axiom
+        self.numIters = numIters
+        endString = ""
+        lSystem = []
+        for i in range(self.numIters):
+            endString = processString(startString)
+            lSystem.append(endString)
+            startString = endString
+
+        self.lSys = endString
+
+    def forwardStep(self):
+        self.currentPos = self.currentPos.__add__(self.currentDir)
+
+    def turtleInterpretation(self, ch):
+        global leafCount, blossomCount
+        if(ch == "F"):
+            self.forwardStep()
+            self.points.append(self.currentPos)
+            # self.directions.append(self.currentDir)
+        if(ch == "-"):
+            # Rotate -X (yaw left)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(1.0, 0.0, 0.0))
+            self.currentDir = self.currentDir.rotateBy(rotAxis, pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == "+"):
+            # Rotate +X (yaw right)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(1.0, 0.0, 0.0))
+            self.currentDir = self.currentDir.rotateBy(
+                rotAxis, -1*pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == "^"):
+            # Rotate +Y (roll right)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(0.0, 1.0, 0.0))
+            self.currentDir = self.currentDir.rotateBy(rotAxis, 1*pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == "&"):
+            # Rotate -Y (roll left)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(0.0, 1.0, 0.0))
+            self.currentDir = self.currentDir.rotateBy(
+                rotAxis, -1*pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == "<"):
+            # Rotate +Z (pitch down)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(0.0, 0.0, 1.0))
+            self.currentDir = self.currentDir.rotateBy(rotAxis, 1*pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == ">"):
+            # Rotate -Z (pitch up)
+            randAngle = random.randint(2, 8)
+            rotAxis = getRotationAxis(
+                self.currentDir, dt.Vector(0.0, 0.0, 1.0))
+            self.currentDir = self.currentDir.rotateBy(
+                rotAxis, -1*pi/randAngle)
+            self.currentDir = self.currentDir.normal()
+        if(ch == "["):
+            copyPoints = copyList(self.points)
+            self.curvesStack.append(copyPoints)
+            self.positionStack.append(self.currentPos)
+            self.directionStack.append(self.currentDir)
+            self.points *= 0
+            self.points.append(self.currentPos)
+        if(ch == "]"):
+            copyPoints = copyList(self.points)
+            self.curves.append(copyPoints)
+            self.points *= 0
+            self.points = self.curvesStack.pop()
+            self.currentPos = self.positionStack.pop()
+            self.currentDir = self.directionStack.pop()
+        if(ch == "L"):
+            pm.duplicate("leaf_geo_mb:pPlaneShape1",
+                         name="leaf" + str(leafCount))
+            pm.select("leaf" + str(leafCount))
+            pm.move(self.currentPos.x, self.currentPos.y, self.currentPos.z)
+            pm.rotate("leaf" + str(leafCount), [str(random.randint(0, 360)) + "deg", str(
+                random.randint(0, 360)) + "deg", str(random.randint(0, 360)) + "deg"])
+            pm.scale(0.05, 0.05, 0.05)
+            self.leafs.append("leaf" + str(leafCount))
+            leafCount = leafCount+1
+        if(ch == "B"):
+            pm.duplicate("blossom_geo_mb:polySurfaceShape2",
+                         name="blossom" + str(blossomCount))
+            pm.select("blossom" + str(blossomCount))
+            pm.move(self.currentPos.x, self.currentPos.y, self.currentPos.z)
+            pm.rotate("blossom" + str(blossomCount), [str(random.randint(0, 360)) + "deg", str(
+                random.randint(0, 360)) + "deg", str(random.randint(0, 360)) + "deg"])
+            pm.scale(0.2, 0.2, 0.2)
+            self.flowers.append("blossom" + str(blossomCount))
+            blossomCount = blossomCount+1
+        if(ch == "s"):
+            self.curves.append(self.points)
+
+    def createCurve(self):
+        for ch in self.lSys:
+            self.turtleInterpretation(ch)
+
+    def createMesh(self, j):
+        for i in range(len(self.curves)):
+            if(len(self.curves[i]) == 2):
+                pm.curve(name="curve{}".format(j) +
+                         "_" + str(i), p=self.curves[i], d=1)
+                pm.circle(name="surface{}".format(j) + "_" + str(i),
+                          nr=self.curves[i][1] - self.curves[i][0], c=self.curves[i][0], r=random.uniform(0.01, 0.05))
+                temp = pm.extrude("surface{}".format(j) + "_" + str(i), "curve{}".format(
+                    j) + "_" + str(i), et=2, pivot=self.curves[i][0], po=1, dl=0.5)
+                self.polys.append(temp)
+            else:
+                pm.curve(name="curve{}".format(j) +
+                         "_" + str(i), p=self.curves[i], d=1)
+                pm.circle(name="surface{}".format(j) + "_" + str(i),
+                          nr=self.curves[i][1] - self.curves[i][0], c=self.curves[i][0], r=0.1)
+                temp = pm.extrude("surface{}".format(j) + "_" + str(i), "curve{}".format(
+                    j) + "_" + str(i), et=2, pivot=self.curves[i][0], scale=0.5, po=1, dl=0.5)
+                self.polys.append(temp)
 
 
 def applyRules(lhch):
     rhch = ""
     if(lhch == "F"):
-        rhch = "^F[&+FL]F[->FL][&FLB]"  # Rule 1
+        rhch = "^F[&+FL]F[->FL][&FLB]"
     if(lhch == "-"):
         rhch = "-"
     if(lhch == "+"):
@@ -68,87 +200,7 @@ def applyRules(lhch):
 
 # How the 'turtle' should interpretend the characters
 
-
-def turtleInterpretation(ch):
-    global currentPos, currentDir, points, leafCount, blossomCount, leafGeo, blossomGeo
-    if(ch == "F"):
-        forwardStep()
-        points.append(currentPos)
-        directions.append(currentDir)
-    if(ch == "-"):
-        # Rotate -X (yaw left)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(1.0, 0.0, 0.0))
-        currentDir = currentDir.rotateBy(rotAxis, pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == "+"):
-        # Rotate +X (yaw right)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(1.0, 0.0, 0.0))
-        currentDir = currentDir.rotateBy(rotAxis, -1*pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == "^"):
-        # Rotate +Y (roll right)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(0.0, 1.0, 0.0))
-        currentDir = currentDir.rotateBy(rotAxis, 1*pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == "&"):
-        # Rotate -Y (roll left)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(0.0, 1.0, 0.0))
-        currentDir = currentDir.rotateBy(rotAxis, -1*pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == "<"):
-        # Rotate +Z (pitch down)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(0.0, 0.0, 1.0))
-        currentDir = currentDir.rotateBy(rotAxis, 1*pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == ">"):
-        # Rotate -Z (pitch up)
-        randAngle = random.randint(2, 8)
-        rotAxis = getRotationAxis(currentDir, dt.Vector(0.0, 0.0, 1.0))
-        currentDir = currentDir.rotateBy(rotAxis, -1*pi/randAngle)
-        currentDir = currentDir.normal()
-    if(ch == "["):
-        copyPoints = copyList(points)
-        curvesStack.append(copyPoints)
-        positionStack.append(currentPos)
-        directionStack.append(currentDir)
-        points *= 0
-        points.append(currentPos)
-    if(ch == "]"):
-        copyPoints = copyList(points)
-        curves.append(copyPoints)
-        points *= 0
-        points = curvesStack.pop()
-        currentPos = positionStack.pop()
-        currentDir = directionStack.pop()
-    if(ch == "L"):
-        pm.duplicate("leaf_geo_mb:pPlaneShape1",
-                     name="leaf" + str(leafCount))
-        pm.select("leaf" + str(leafCount))
-        pm.move(currentPos.x, currentPos.y, currentPos.z)
-        pm.rotate("leaf" + str(leafCount), [str(random.randint(0, 360)) + "deg", str(
-            random.randint(0, 360)) + "deg", str(random.randint(0, 360)) + "deg"])
-        pm.scale(0.05, 0.05, 0.05)
-        leafs.append("leaf" + str(leafCount))
-        leafCount = leafCount+1
-    if(ch == "B"):
-        pm.duplicate("blossom_geo_mb:polySurfaceShape2",
-                     name="blossom" + str(blossomCount))
-        pm.select("blossom" + str(blossomCount))
-        pm.move(currentPos.x, currentPos.y, currentPos.z)
-        pm.rotate("blossom" + str(blossomCount), [str(random.randint(0, 360)) + "deg", str(
-            random.randint(0, 360)) + "deg", str(random.randint(0, 360)) + "deg"])
-        pm.scale(0.2, 0.2, 0.2)
-        flowers.append("blossom" + str(blossomCount))
-        blossomCount = blossomCount+1
-    if(ch == "s"):
-        curves.append(points)
-
-        # Process string so the rules can be applied for each character
+    # Process string so the rules can be applied for each character
 
 
 def getRotationAxis(dir, dir2):
@@ -170,32 +222,7 @@ def processString(oldStr):
 
     return newstr
 
-
-def createLSystem(numIters, axiom, lSystem_):
-    startString = axiom
-    endString = ""
-    lSystem = lSystem_
-    for i in range(numIters):
-        endString = processString(startString)
-        lSystem.append(endString)
-        startString = endString
-
-    return endString
-
-# Calculate new position
-
-
-def forwardStep():
-    global currentPos
-    currentPos = currentPos.__add__(currentDir)
-
 # Create the mesh, loops through all the characters
-
-
-def createCurve(arr, numIters):
-    global currentDir, currentPos
-    for ch in arr[numIters-1]:
-        turtleInterpretation(ch)
 
 
 def loadGeo(fileName):
@@ -203,41 +230,6 @@ def loadGeo(fileName):
     pm.importFile(dir_path+"/Objects/" + fileName, i=True,
                   namespace=str(fileName))
 
-
-def createMesh(crv):
-    for i in range(len(crv)):
-        if(len(crv[i]) == 2):
-            pm.curve(name="curve{}".format(i), p=crv[i], d=1)
-            pm.circle(name="surface{}".format(i),
-                      nr=crv[i][1] - crv[i][0], c=crv[i][0], r=random.uniform(0.01, 0.05))
-            temp = pm.extrude("surface{}".format(i), "curve{}".format(
-                i), et=2, pivot=crv[i][0], po=1, dl=0.5)
-            polys.append(temp)
-        else:
-            pm.curve(name="curve{}".format(i), p=crv[i], d=1)
-            crl = pm.circle(name="surface{}".format(i),
-                            nr=crv[i][1] - crv[i][0], c=crv[i][0], r=0.1)
-            temp = pm.extrude("surface{}".format(i), "curve{}".format(
-                i), et=2, pivot=crv[i][0], scale=0.5, po=1, dl=0.5)
-            polys.append(temp)
-
-
-loadGeo("leaf_geo.mb")
-loadGeo("blossom_geo.mb")
-n = 4
-lSystem = []
-polys = []
-createLSystem(n, "F s", lSystem)
-createCurve(lSystem, n)
-createMesh(curves)
-pm.delete("leaf_geo_mb:pPlaneShape1")
-pm.delete("blossom_geo_mb:polySurfaceShape2")
-
-TreePoly = pm.polyUnite(polys, name="TreePoly")
-LeafPoly = pm.polyUnite(leafs, name="LeafPoly")
-BlossomPoly = pm.polyUnite(flowers, name="BlossomPoly")
-pm.polyReduce(TreePoly, p=90)
-pm.polyReduce(LeafPoly, p=50)
 
 # -----------------------Shaders----------------------------------------------
 
@@ -274,13 +266,49 @@ def createShader(obj, texturePathColor, texturePathBump):
 # ----------------------------------------------------------------------------
 
 
-# Add textures folder to where script is being run from within maya
-dir_path = cmds.internalVar(userScriptDir=1)
-createShader(TreePoly, dir_path + "/Shaders/TreeBark/color.tif",
-             dir_path + "Shaders/TreeBark/bump.tif")
+loadGeo("leaf_geo.mb")
+loadGeo("blossom_geo.mb")
 
-createShader(LeafPoly, dir_path + "/Shaders/Leaf/grass.tif",
-             dir_path + "Shaders/Leaf/Leaf_Ulmus_Bump.tga")
-createShader(BlossomPoly, dir_path + "/Shaders/Blossom/iridescent_paper.tif",
-             dir_path + "Shaders/Leaf/Leaf_Ulmus_Bump.tga")
-# print(LeafPoly)
+n = 4
+Trees = []
+myTree1 = Tree(dt.Vector(random.randint(-5, 5),
+                         0, random.randint(-5, 5)))
+myTree2 = Tree(dt.Vector(random.randint(-5, 5),
+                         0, random.randint(-5, 5)))
+
+myTree3 = Tree(dt.Vector(random.randint(-5, 5),
+                         0, random.randint(-5, 5)))
+myTree4 = Tree(dt.Vector(random.randint(-5, 5),
+                         0, random.randint(-5, 5)))
+myTree5 = Tree(dt.Vector(random.randint(-5, 5),
+                         0, random.randint(-5, 5)))
+Trees.append(myTree1)
+Trees.append(myTree2)
+Trees.append(myTree3)
+Trees.append(myTree4)
+Trees.append(myTree5)
+
+
+dir_path = cmds.internalVar(userScriptDir=1)
+
+for i in range(len(Trees)):
+    Trees[i].createLSystem(n, "F s")
+    Trees[i].createCurve()
+    Trees[i].createMesh(i)
+    TreePoly = pm.polyUnite(Trees[i].polys, name="TreePoly")
+    LeafPoly = pm.polyUnite(Trees[i].leafs, name="LeafPoly")
+    BlossomPoly = pm.polyUnite(Trees[i].flowers, name="BlossomPoly")
+    pm.polyReduce(TreePoly, p=90)
+    pm.polyReduce(LeafPoly, p=50)
+
+    # Add textures folder to where script is being run from within maya
+    createShader(TreePoly, dir_path + "/Shaders/TreeBark/color.tif",
+                 dir_path + "Shaders/TreeBark/bump.tif")
+
+    createShader(LeafPoly, dir_path + "/Shaders/Leaf/grass.tif",
+                 dir_path + "Shaders/Leaf/Leaf_Ulmus_Bump.tga")
+    createShader(BlossomPoly, dir_path + "/Shaders/Blossom/iridescent_paper.tif",
+                 dir_path + "Shaders/Leaf/Leaf_Ulmus_Bump.tga")
+
+pm.delete("leaf_geo_mb:pPlaneShape1")
+pm.delete("blossom_geo_mb:polySurfaceShape2")
